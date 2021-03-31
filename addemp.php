a9d6d5b8753fba819021f03a6dda9e9df10d2a20
JAIMINMISTRY12 <78132921+JAIMINMISTRY12@users.noreply.github.com>
2021-03-31 11:44:05 +0530

    Add files via upload

---
+++ addemp.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+   
+
+    <!-- Title Page-->
+    <title>Add Employee | Employee Management System</title>
+
+    <!-- Icons font CSS-->
+    <link href="" rel="stylesheet" media="all">
+    <link href="" rel="stylesheet" media="all">
+    <!-- Font special for pages-->
+    <link href="">
+
+    <!-- Vendor CSS-->
+    <link href="" rel="stylesheet" media="all">
+    <link href="" rel="stylesheet" media="all">
+
+    <!-- Main CSS-->
+    <link href="" rel="stylesheet" media="all">
+</head>
+
+<body>
+    <header>
+        <nav>
+            <h1>EMS</h1>
+            <ul id="navli">
+                <li><a class="homeblack" href="aloginwel.php">HOME</a></li>
+                <li><a class="homered" href="addemp.php">Add Employee</a></li>
+                <li><a class="homeblack" href="viewemp.php">View Employee</a></li>
+                <li><a class="homeblack" href="assign.php">Assign Project</a></li>
+                <li><a class="homeblack" href="assignproject.php">Project Status</a></li>
+                <li><a class="homeblack" href="salaryemp.php">Salary Table</a></li> 
+                <li><a class="homeblack" href="empleave.php">Employee Leave</a></li>
+                <li><a class="homeblack" href="alogin.html">Log Out</a></li>
+            </ul>
+        </nav>
+    </header>
+    
+    <div class="divider"></div>
+
+
+
+
+    <div class="page-wrapper bg-blue p-t-100 p-b-100 font-robo">
+        <div class="wrapper wrapper--w680">
+            <div class="card card-1">
+                <div class="card-heading"></div>
+                <div class="card-body">
+                    <h2 class="title">Registration Info</h2>
+                    <form action="process/addempprocess.php" method="POST" enctype="multipart/form-data">
+
+
+                        
+
+                        <div class="row row-space">
+                            <div class="col-2">
+                                <div class="input-group">
+                                     <input class="input--style-1" type="text" placeholder="First Name" name="firstName" required="required">
+                                </div>
+                            </div>
+                            <div class="col-2">
+                                <div class="input-group">
+                                    <input class="input--style-1" type="text" placeholder="Last Name" name="lastName" required="required">
+                                </div>
+                            </div>
+                        </div>
+
+
+
+
+
+                        <div class="input-group">
+                            <input class="input--style-1" type="email" placeholder="Email" name="email" required="required">
+                        </div>
+                        <p>Birthday</p>
+                        <div class="row row-space">
+                            <div class="col-2">
+                                <div class="input-group">
+                                    <input class="input--style-1" type="date" placeholder="BIRTHDATE" name="birthday" required="required">
+                                   
+                                </div>
+                            </div>
+                            <div class="col-2">
+                                <div class="input-group">
+                                    <div class="rs-select2 js-select-simple select--no-search">
+                                        <select name="gender">
+                                            <option disabled="disabled" selected="selected">GENDER</option>
+                                            <option value="Male">Male</option>
+                                            <option value="Female">Female</option>
+                                            <option value="Other">Other</option>
+                                        </select>
+                                        <div class="select-dropdown"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        
+                        <div class="input-group">
+                            <input class="input--style-1" type="number" placeholder="Contact Number" name="contact" required="required" >
+                        </div>
+
+                        <div class="input-group">
+                            <input class="input--style-1" type="number" placeholder="NID" name="nid" required="required">
+                        </div>
+
+                        
+                         <div class="input-group">
+                            <input class="input--style-1" type="text" placeholder="Address" name="address" required="required">
+                        </div>
+
+                        <div class="input-group">
+                            <input class="input--style-1" type="text" placeholder="Department" name="dept" required="required">
+                        </div>
+
+                        <div class="input-group">
+                            <input class="input--style-1" type="text" placeholder="Degree" name="degree" required="required">
+                        </div>
+
+                        <div class="input-group">
+                            <input class="input--style-1" type="number" placeholder="Salary" name="salary">
+                        </div>
+
+                        <div class="input-group">
+                            <input class="input--style-1" type="file" placeholder="file" name="file">
+                        </div>
+
+
+
+
+
+
+
+                        <div class="p-t-20">
+                            <button class="btn btn--radius btn--green" type="submit">Submit</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Jquery JS-->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <!-- Vendor JS-->
+    <script src=""></script>
+    <script src=""></script>
+    <script src=""></script>
+
+    <!-- Main JS-->
+    <script src=""></script>
+
+</body>
+
+</html>
---
+++ eloginwell.php
@@ -0,0 +1,21 @@
+<?php 
+	$id = (isset($_GET['id']) ? $_GET['id'] : '');
+	require_once ('process/dbh.php');
+	 $sql1 = "SELECT * FROM `employee` where id = '$id'";
+	 $result1 = mysqli_query($conn, $sql1);
+	 $employeen = mysqli_fetch_array($result1);
+	 $empName = ($employeen['firstName']);
+
+	$sql = "SELECT id, firstName, lastName,  points FROM employee, rank WHERE rank.eid = employee.id order by rank.points desc";
+	$sql1 = "SELECT `pname`, `duedate` FROM `project` WHERE eid = $id and status = 'Due'";
+
+	$sql2 = "Select * From employee, employee_leave Where employee.id = $id and employee_leave.id = $id order by employee_leave.token";
+
+	$sql3 = "SELECT * FROM `salary` WHERE id = $id";
+
+//echo "$sql";
+$result = mysqli_query($conn, $sql);
+$result1 = mysqli_query($conn, $sql1);
+$result2 = mysqli_query($conn, $sql2);
+$result3 = mysqli_query($conn, $sql3);
+?>
